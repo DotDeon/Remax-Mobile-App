3682f8c03b99a58872092f8ec17eaf2cd08cca5b
Oh8Digital <deon@oh8digital.co.za>
2019-10-15 11:31:55 +0200

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:npa/Screens/Login/index.dart';
+import 'package:remaxinfoagent/Screens/Login/index.dart';
 import 'package:remaxinfoagent/main.dart';
 
 void main() {
